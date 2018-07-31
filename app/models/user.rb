e69e160a48b55aff7a5e1494e0faaacd0efbc338
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-07-31 13:28:22 +0200

    Second commit and pushing to Heroku

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	validates :username, presence: true, uniqueness: true
+    validates :username, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+	validates :bio, presence: true
+
+	validates :email, presence: true, email: true, uniqueness:true
+end
