edf95ca30fdc80ec9cf244e2b7a5402db1464571
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-07-31 18:05:55 +0200

    commit de mes superbes readme

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'users/new', to: 'users#new'
   post 'users', to: 'users#create'
-  get 'users/:id', to: 'user#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
