86a771fb3290193d765213a019eb02b2bd12c555
Cathleen HENNINOT <cathleenhenninot@MacBook-Pro-de-Cathleen.local>
2018-07-31 22:35:12 +0200

    les formes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,23 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-    @user.save
-    redirect_to user_path(@user)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :create
+    end
   end
 
   def show
       @user = User.find(params[:id])	
   end
 
-private
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :bio)
   end
 end
+
+
